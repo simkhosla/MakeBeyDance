049ada4b4713119fcb6883953f92df4e24060950
Simran Khosla <email.simrankhosla@gmail.com>
2015-11-06 16:20:07 -0600

    added spotify music embed yay

--- app.js
+++ app.js
@@ -4,28 +4,43 @@ $(document).ready(function(){
 
 console.log('Everything has loaded.');
 
+  var bey = $('.box1');
+
   $('li').fadeIn(2000);
 
   $('#btnRight').click(function(){
     console.log('mybuttonisworking');
-    $('.box1').css("background-image", "url(beyRight.png)");
-    $('.box1').animate({'left':'+=50px'}, 'slow');
+    bey.css("background-image", "url(beyRight.png)");
+    bey.animate({'left':'+=50px'}, 'slow');
   });
 
   $('#btnLeft').click(function(){
     console.log('mybuttonisworking');
-    $('.box1').css("background-image", "url(beyLeft.png)");
-    $('.box1').animate({'left':'-=50px'}, 'slow');
+    bey.css("background-image", "url(beyLeft.png)");
+    bey.animate({'left':'-=50px'}, 'slow');
   });
 
 
   $('#btnShake').click(function(){
     console.log('mybuttonisworking');
-    $('.box1').css("background-image", "url(beybutt.gif)");
+    bey.css("background-image", "url(beybutt.gif)");
   });
 
-  
+  $( "html" ).keyup(function( event ) {
+  if ( event.keyCode == 37 ) {
+    bey.css("background-image", "url(beyLeft.png)");
+    bey.animate({'left':'-=50px'}, 'slow');
+  };
+    console.log(event.keyCode);
+  });
 
+  $( "html" ).keyup(function( event ) {
+  if ( event.keyCode == 39 ) {
+    bey.css("background-image", "url(beyRight.png)");
+    bey.animate({'left':'+=50px'}, 'slow');
+  };
+    console.log(event.keyCode);
+  });
 
 
 
